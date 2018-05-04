ada16740e23b72eadd0833d8ea67978377ac4130
amit-kumar27 <amit27kmr@gmail.com>
2018-05-04 14:24:31 +0530

    Create app.module.ts

---
+++ src/client/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import {providers, imports, declarations} from "./module-node.config";
+
+@NgModule({
+  imports: imports(),
+  declarations: declarations(),
+  providers: providers(),
+  exports: declarations(),
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+})
+
+export class AppModule {
+  constructor() {
+
+  }
+}
